82ba8bd6e8751c3a552c13a8eb0a3a46ee77b4c5
njrsouza <njrsouza@outlook.com>
2026-02-26 00:10:40 -0300

    Modal para cada pokemon quando aperta em cima, com informações sobre suas stats.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-// Primeiro Passo - Realizar a requisição
 
 var offset = 0;
 var limit = 10;
 
-url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-
-listPokemons = document.getElementById('list-pokemons');
+const listPokemons = document.getElementById('list-pokemons');
 loadMore = document.getElementById('LoadMore');
+const pokemons = document.querySelectorAll('.pokemon');
+
+
 
 
 
 getPokemons = (limit, offset) => {
-    url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
     fetch(url)
         .then((response) => response.json()) // Transformamos a resposta em JSON -> Primeiras informações 
@@ -28,9 +28,6 @@ getPokemons = (limit, offset) => {
 
 }
 
-
-
-
 getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
@@ -38,7 +35,7 @@ getPokemonDetail = (pokemon) => {
 
 pokemonToHTML = (pokemon) => {
     return `<li class="col">
-                <div class="${pokemon.types[0].type.name}  p-3 d-flex flex-column rounded text-white h-100 gap-2 overflow-hidden">
+                <div class="${pokemon.types[0].type.name} pokemon p-3 d-flex flex-column rounded text-white h-100 gap-2 overflow-hidden" data-id="${pokemon.id}">
 
                     <div class="d-flex flex-column">
                         <span class="number align-self-end opacity-25">#${pokemon.id}</span>
@@ -62,6 +59,76 @@ pokemonToHTML = (pokemon) => {
 loadMore.addEventListener('click', () => {
     getPokemons(limit, offset)
     offset += limit
-
-
 })
+
+listPokemons.addEventListener('click', (event) => {
+    const cardClicado = event.target.closest('.pokemon')
+
+    if (!cardClicado) return;
+
+    const pokemonId = cardClicado.dataset.id;
+
+    abrirDetalhesPokemon(pokemonId)
+})
+
+const modal = document.getElementById('modal-pokemon')
+
+
+function abrirDetalhesPokemon(id) {
+    console.log("Buscando dados do Pokémon número: " + id);
+
+    let urlPokemon = `https://pokeapi.co/api/v2/pokemon/${id}`
+    fetch(urlPokemon)
+        .then((response) => response.json())
+        .then((pokemon) => {
+            modal.innerHTML = getDetails(pokemon)
+            const buttonClose = document.getElementById('close-button')
+            buttonClose.addEventListener('click', () => modal.close())
+        })
+        .catch((error) => console.log(error))
+
+
+
+    modal.showModal();
+}
+
+
+getDetails = (pokemon) => {
+    return `<div class="modal-content">
+
+            <div class="infos-poke d-flex justify-content-between">
+                <div>
+                    <h1 class="name-pokemon">${pokemon.name}</h1>
+                    <ul class="types-pokemon d-flex gap-3 list-unstyled">
+                        ${pokemon.types.map((type) => `<li class="type mb-1 border-0 text-center p-2 rounded-pill bg-dark bg-opacity-25"
+                                >${type.type.name}</li>`).join('')}
+                    </ul>
+                </div>
+
+                <span class="id">#${pokemon.id}</span>
+            </div>
+
+            <img src="${pokemon.sprites.other.dream_world.front_default}"
+                alt="${pokemon.name}" class="img-fluid mb-4">
+
+            <ul class="stats d-flex flex-column justify-content-center align-items-start p-0">
+
+               
+
+                ${pokemon.stats.map((stat) => `<div class="stat d-flex align-items-center gap-3">
+                    <span class="name-stat">${stat.stat.name}</span>
+                    <span><b>${stat.base_stat} </b></span>
+                    <progress value="${stat.base_stat}" max="255"></progress>
+                </div>`).join('')}
+
+
+            </ul>
+
+        </div>
+
+        <button id="close-button" class="p-2 rounded-pill bg-transparent">Fechar</button>`
+}
+
+
+
+
